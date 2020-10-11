e96bd1574a48c1208cf451ccf572b29186eececf
beharavenkatasatyaprasad <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-10-12 03:42:17 +0530

    Add files via upload

---
+++ boomcars/v0.1/js/script.js
@@ -0,0 +1,25 @@
+
+        var mybutton = document.getElementById("myBtn");
+        
+        window.onscroll = function() {scrollFunction()};
+        
+        function scrollFunction() {
+          if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+          } 
+          else {
+            mybutton.style.display = "none";
+          }
+        }
+        
+        function topFunction() {
+          document.body.scrollTop = 0;
+          document.documentElement.scrollTop = 0;
+        }
+
+        function carfun(){
+          alert("You should have to work hard to dream for a car like this.");
+        }
+        function welcome(){
+          alert("Hello There!!! Welcome to the page")
+        }
